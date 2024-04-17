e76326b99f09ecbaabefb5795975e2b690855a0d
Andrew Pareles <andrewpareles@gmail.com>
2024-04-16 21:27:57 -0400

    +

--- client/client.js
+++ client/client.js
@@ -2,8 +2,9 @@
 const io = require('socket.io-client');
 const { vec } = require('../common/vector.js');
 
-const ADDRESS = 'ws://192.168.1.163:3001';
+// const ADDRESS = 'ws://192.168.1.163:3001';
 // const ADDRESS = 'wss://reelerio.herokuapp.com/';
+const ADDRESS = 'wss://splendorous-pithivier-ee9c1b.netlify.app/';
 const socket = io(ADDRESS);
 
 /** ---------- GAME CONSTANTS ----------
